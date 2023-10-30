436cacda57e23adf38e45fc3b54a12f8169387de
20220303 <100846102+20220303@users.noreply.github.com>
2023-10-30 14:20:09 +0800

    add a getMax function

--- gcd.java
+++ gcd.java
@@ -1,25 +1,12 @@
 public class Test{
-	public int abs（int a,int b）{
-		if(a>b){
-			a=a-b;
-		}else{
-			a=b-a;
-		}
-		return a;
-	}
-
-	
-	public int[] twoSum(int[] nums, int target) {
-		int len = nums.length;
-		for (int i = 0; i < len; ++i) {
-		    for (int j = i + 1; j < len; ++j) {
-			if (nums[i] + nums[j] == target) {
-			    return new int[]{i, j};
+	public static int getMax(int[] arr) {
+		int max = arr[0];
+		for(int i = 1;i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
 			}
-		    }
 		}
-		return new int[0];
+		return max;
 	}
-
 }
 
