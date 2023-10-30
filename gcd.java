7c3728ee4312a19275a91ea6afe9a2abad32d209
20220303 <100846102+20220303@users.noreply.github.com>
2023-10-30 13:39:22 +0800

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,12 +1,11 @@
 public class Test{
-	public int gcd（int a,int b）{
-		while(b!=0){
-			if(a>b){
-				a=a-b;
-			}else{
-				b=b-a;
-			}
+	public int abs（int a,int b）{
+		if(a>b){
+			a=a-b;
+		}else{
+			a=b-a;
 		}
+		return a;
 	}
 }
 
