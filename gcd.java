8115b6d8f3e3aa197c078d6aba99d1ed69822ad5
20220303 <100846102+20220303@users.noreply.github.com>
2023-10-30 14:26:35 +0800

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,12 +1,4 @@
 public class Test{
-  	public static int getMax(int[] arr) {
-		int max = arr[0];
-		for(int i = 1;i < arr.length; i++) {
-			if (max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		return max;
-	}
+  	
 }
 
