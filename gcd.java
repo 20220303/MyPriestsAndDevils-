b67e2889db07adfde88b41a151241c56563221bf
20220303 <100846102+20220303@users.noreply.github.com>
2023-10-30 14:22:35 +0800

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,12 +1,4 @@
 public class Test{
-	public static int getMin(int[] arr) {
-		int min= arr[0];
-		for(int i = 1;i < arr.length; i++) {
-			if (min> arr[i]) {
-				min= arr[i];
-			}
-		}
-		return min;
-	}
+
 }
 
