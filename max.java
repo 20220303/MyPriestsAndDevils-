7534d593277f21d4371aa429ab6df1db8cb3940a
20220303 <100846102+20220303@users.noreply.github.com>
2023-10-30 15:55:32 +0800

    Update max.java

--- max.java
+++ max.java
@@ -1,19 +1,1 @@
-public static int getMax(int[] arr) {
-		int max = arr[0];
-		for(int i = 1;i < arr.length; i++) {
-			if (max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		return max;
-	}
 
-public static int getMin(int[] arr) {
-		int min= arr[0];
-		for(int i = 1;i < arr.length; i++) {
-			if (min> arr[i]) {
-				min= arr[i];
-			}
-		}
-		return min;
-	}
