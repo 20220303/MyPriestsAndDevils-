07f8a99733cd4e56008b891dbb18f1d0e1e5a07f
20220303 <100846102+20220303@users.noreply.github.com>
2023-11-02 11:25:38 +0800

    Update max.java

--- max.java
+++ max.java
@@ -1,12 +1,11 @@
 public void btnSendRequest(ActionEvent actionEvent) {
     TreeItem<String> selectedItem = (TreeItem<String>) treePaths.getSelectionModel().getSelectedItem();
-    String message = selectedItem.getValue() + " not implemented yet"
     switch (selectedItem.getValue()) {
         case "GET":
             Platform.runLater(() -> getRequest());
         case "POST":
             Platform.runLater(() -> postRequest());
         default:
-            log.error(message);
+            log.error(selectedItem.getValue() + " not implemented yet");
     }
 }
